a217412fe55b446b3f821f8dacf00a1c75cd5c58
syalicheff <yalicheff.sebastien@gmail.com>
2022-06-23 12:24:05 +0200

    Auth Providers

--- client/index.php
+++ client/index.php
@@ -1,51 +1,189 @@
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js" integrity="sha384-QJHtvGhmr9XOIpI6YVutG+2QOK9T+ZnN4kzFN1RtK3zEFEIsxhlmWl5/YESvpZ13" crossorigin="anonymous"></script>
 <?php
 
-define("CLIENT_ID", '67dc2be521bec2ff862d3ab057de216b');
-define("CLIENT_SECRET", '04054cf433eeb3976252c81b6d657fda');
+define('OAUTH_CLIENT_ID', '621f59c71bc35');
+define('OAUTH_CLIENT_SECRET', '621f59c71bc36');
+define('FACEBOOK_CLIENT_ID', '1154946705081703');
+define('FACEBOOK_CLIENT_SECRET', '4e9835eeba3388b55bcb02b2cd27d522');
+define('LINKEDIN_CLIENT_ID', '78ih5ts4jtua92');
+define('LINKEDIN_CLIENT_SECRET','gTIo9xTOquRmcxx1');
+define('GOOGLE_CLIENT_ID', '44229373992-45hc7opqdtd7p2iq1bjr1c8g5rga6s2r.apps.googleusercontent.com');
+define('GOOGLE_CLIENT_SECRET','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');
 
-// Create a login page with a link to oauth
+
 function login()
 {
-    $queryParams = http_build_query([
-        "state"=>bin2hex(random_bytes(16)),
-        "client_id"=> CLIENT_ID,
-        "scope"=>"profile",
-        "redirect_uri"=>"http://localhost:8081/oauth_success",
+    $queryParams= http_build_query([
+        'client_id' => OAUTH_CLIENT_ID,
+        'redirect_uri' => 'http://localhost:8081/callback',
+        'response_type' => 'code',
+        'scope' => 'basic',
+        "state" => bin2hex(random_bytes(16))
     ]);
-    echo "<a href=\"http://localhost:8080/auth?{$queryParams}\">Login with Oauth-Server</a>";
+    echo "
+        <form action='/callback' method='post'>
+            <input type='text' name='username'/>
+            <input type='password' name='password'/>
+            <input type='submit' value='Login'/>
+        </form>
+    ";
+    echo "<a class='btn btn-danger' href =\"http://localhost:8080/auth?{$queryParams}\">OauthServer</a>";
+
+    $queryParams= http_build_query([
+        'client_id' => FACEBOOK_CLIENT_ID,
+        'redirect_uri' => 'http://localhost:8081/fb_callback',
+        'response_type' => 'code',
+        'scope' => 'public_profile,email',
+        "state" => bin2hex(random_bytes(16))
+    ]);
+    echo "<a class='btn btn-danger'= href =\"https://www.facebook.com/v2.10/dialog/oauth?{$queryParams}\">Facebook</a>";
+
+    $queryParams= http_build_query([
+        'response_type' => 'code',
+        'client_id' => LINKEDIN_CLIENT_ID,
+        'redirect_uri' => 'http://localhost:8081/lkdn_callback',
+        'state' => bin2hex(random_bytes(16)),
+        'scope' => 'r_liteprofile r_emailaddress'
+    ]);       
+    // Design pattern de factory 
+    // Class abstraite provider provider interface
+    //echo "https://www.linkedin.com/oauth/v2/authorization?{$queryParams}";
+    echo "<a class='btn btn-danger' href=\"https://www.linkedin.com/oauth/v2/authorization?{$queryParams}\">Linkedin</a>";
+
+    $queryParams= http_build_query([
+        'scope' => 'email',
+        'response_type' => 'code',
+        'client_id' => GOOGLE_CLIENT_ID,
+        'redirect_uri' => 'http://localhost:8081/google_callback',
+        'state' => bin2hex(random_bytes(16))
+    ]);
+        // 'response_type' => 'code',
+       
+    // Design pattern de factory 
+    // Class abstraite provider provider interface
+    //echo "https://www.linkedin.com/oauth/v2/authorization?{$queryParams}";
+    echo "<a class='btn btn-danger' href=\"https://accounts.google.com/o/oauth2/v2/auth?{$queryParams}\">Google</a>";
+    echo "<br>";
 }
 
-// get token from code then get user info
+// Exchange code for token then get user info
 function callback()
 {
-    $code = $_GET['code'];
-    $state = $_GET['state'];
-    $queryParams = http_build_query([
-        "grant_type"=>"authorization_code",
-        "code"=>$code,
-        "redirect_uri"=>"http://localhost:8081/oauth_success",
-        "client_id"=>CLIENT_ID,
-        "client_secret"=>CLIENT_SECRET,
-    ]);
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+        ["username" => $username, "password" => $password] = $_POST;
+        $specifParams = [
+            'username' => $username,
+            'password' => $password,
+            'grant_type' => 'password',
+        ];
+    } else {
+        ["code" => $code, "state" => $state] = $_GET;
+
+        $specifParams = [
+            'code' => $code,
+            'grant_type' => 'authorization_code',
+        ];
+    }
+
+    $queryParams = http_build_query(array_merge([
+        'client_id' => OAUTH_CLIENT_ID,
+        'client_secret' => OAUTH_CLIENT_SECRET,
+        'redirect_uri' => 'http://localhost:8081/callback',
+    ], $specifParams));
     $response = file_get_contents("http://server:8080/token?{$queryParams}");
-    if (!$response) {
-        echo $http_response_header;
-        return;
-    }
-    ["access_token" => $token] = json_decode($response, true);
-
-
+    $token = json_decode($response, true);
+    
     $context = stream_context_create([
-        "http"=>[
-            "header"=>"Authorization: Bearer {$token}"
-        ]
-    ]);
+        'http' => [
+            'header' => "Authorization: Bearer {$token['access_token']}"
+            ]
+        ]);
     $response = file_get_contents("http://server:8080/me", false, $context);
-    if (!$response) {
-        echo $http_response_header;
-        return;
-    }
-    var_dump(json_decode($response, true));
+    $user = json_decode($response, true);
+    echo "Hello {$user['lastname']} {$user['firstname']}";
+}
+
+function fbcallback()
+{
+    ["code" => $code, "state" => $state] = $_GET;
+
+    $specifParams = [
+            'code' => $code,
+            'grant_type' => 'authorization_code',
+        ];
+
+    $queryParams = http_build_query(array_merge([
+        'client_id' => FACEBOOK_CLIENT_ID,
+        'client_secret' => FACEBOOK_CLIENT_SECRET,
+        'redirect_uri' => 'http://localhost:8081/fb_callback',
+    ], $specifParams));
+    $response = file_get_contents("https://graph.facebook.com/v2.10/oauth/access_token?{$queryParams}");
+    $token = json_decode($response, true);
+    
+    $context = stream_context_create([
+        'http' => [
+            'header' => "Authorization: Bearer {$token['access_token']}"
+            ]
+        ]);
+    $response = file_get_contents("https://graph.facebook.com/v2.10/me", false, $context);
+    $user = json_decode($response, true);
+    echo "Hello {$user['name']}";
+}
+function lkdncallback(){
+    ["code" => $code, "state" => $state] = $_GET;
+
+    $specifParams = [
+            'code' => $code,
+            'grant_type' => 'authorization_code',
+        ];
+
+    $queryParams = http_build_query(array_merge([
+        'client_id' =>LINKEDIN_CLIENT_ID,
+        'client_secret' => LINKEDIN_CLIENT_SECRET,
+        'redirect_uri' => 'http://localhost:8081/lkdn_callback',
+    ], $specifParams));
+    $response = file_get_contents("https://graph.facebook.com/v2.10/oauth/access_token?{$queryParams}");
+    $token = json_decode($response, true);
+    
+    $context = stream_context_create([
+        'http' => [
+            'header' => "Authorization: Bearer {$token['access_token']}"
+            ]
+        ]);
+    $response = file_get_contents("https://graph.facebook.com/v2.10/me", false, $context);
+    $user = json_decode($response, true);
+    echo "Hello {$user['name']}";
+}
+function googlecallback(){
+    var_dump("GOOGLE CONNECTED");
+    var_dump($_GET);
+
+    ["code" => $code, "state" => $state] = $_GET;
+
+    $specifParams = [
+            'code' => $code,
+            'grant_type' => 'authorization_code',
+        ];
+
+    $queryParams = http_build_query(array_merge([
+        'client_id' =>GOOGLE_CLIENT_ID,
+        'client_secret' => GOOGLE_CLIENT_SECRET,
+        'redirect_uri' => 'http://localhost:8081/google_callback',
+    ], $specifParams));
+    echo($queryParams);
+    $response = file_get_contents("https://www.googleapis.com/oauth2/v1/userinfo?alt=json?{$queryParams}");
+    $token = json_decode($response, true);
+    var_dump($token);
+    
+    $context = stream_context_create([
+        'http' => [
+            'header' => "Authorization: Bearer {$token['access_token']}"
+            ]
+        ]);
+    // $response = file_get_contents("https://graph.facebook.com/v2.10/me", false, $context);
+    $user = json_decode($response, true);
+    echo "Hello {$user['name']}";
 }
 
 $route = $_SERVER["REQUEST_URI"];
@@ -53,9 +191,19 @@ switch (strtok($route, "?")) {
     case '/login':
         login();
         break;
-    case '/oauth_success':
+    case '/callback':
         callback();
+        break;
+    case '/fb_callback':
+        fbcallback();
+        break;
+    case '/lkdn_callback':
+        lkdncallback();
+        break;
+    case '/google_callback':
+        googlecallback();
         break;
     default:
         http_response_code(404);
+        break;
 }
--- server/index.php
+++ server/index.php
@@ -1,25 +1,30 @@
 <?php
 
-function readDatabase($filename) {
+function readDatabase($filename)
+{
     return array_map(fn ($line) => json_decode($line, true), file($filename));
 }
 
-function writeDatabase($filename, $data) {
+function writeDatabase($filename, $data)
+{
     file_put_contents(
         $filename,
-        implode("\n", 
+        implode(
+            "\n",
             array_map(fn ($line) => json_encode($line), $data)
         )
     );
 }
 
-function insert($filename, $line) {
+function insert($filename, $line)
+{
     $data = readDatabase($filename);
     $data[] = $line;
     writeDatabase($filename, $data);
 }
 
-function findBy($filename, $criteria) {
+function findBy($filename, $criteria)
+{
     $data = readDatabase($filename);
     $result = array_values(
         array_filter(
@@ -30,27 +35,42 @@ function findBy($filename, $criteria) {
     return count($result) > 0 ? $result[0] : null;
 }
 
-function findAppBy($criteria) {
+function findAppBy($criteria)
+{
     return findBy("./data/apps.db", $criteria);
 }
 
-function findCodeBy($criteria) {
+function findCodeBy($criteria)
+{
     return findBy("./data/codes.db", $criteria);
 }
-function findAppByName($name) {
+function findUserBy($criteria)
+{
+    return findBy("./data/users.db", $criteria);
+}
+function findTokenBy($criteria)
+{
+    return findBy("./data/tokens.db", $criteria);
+}
+function findAppByName($name)
+{
     return findAppBy(['name' => $name]);
 }
-function insertApp($app) {
+function insertApp($app)
+{
     insert('./data/apps.db', $app);
 }
-function insertCode($code) {
+function insertCode($code)
+{
     insert('./data/codes.db', $code);
 }
-function insertToken($token) {
+function insertToken($token)
+{
     insert('./data/tokens.db', $token);
 }
 
-function register() {
+function register()
+{
     ['name' => $name, 'url' => $url, 'redirect_success' => $redirect] = $_POST;
     if (findAppByName($name)) {
         http_response_code(409);
@@ -67,7 +87,8 @@ function register() {
     echo json_encode($app);
 }
 
-function auth() {
+function auth()
+{
     ['client_id'=> $clientId, 'scope' => $scope, 'redirect_uri' => $redirect, 'state' => $state] = $_GET;
     $app = findAppBy(["client_id" => $clientId, 'redirect_success' => $redirect]);
     if (!$app) {
@@ -81,27 +102,57 @@ function auth() {
     echo "<a href='/failed'>Non</a>";
 }
 
-function authSuccess() {
+function authSuccess()
+{
     ['state' => $state, 'client_id' => $clientId] = $_GET;
     $app = findAppBy(["client_id" => $clientId]);
     if (!$app) {
         http_response_code(404);
-        return;
+        throw new \RuntimeException();
     }
     $code = [
         "code" => bin2hex(random_bytes(16)),
         "client_id" => $clientId,
-        "user_id" => bin2hex(random_bytes(16)),
+        "user_id" => 1,
         "expires_at" => time() + 3600,
     ];
     insertCode($code);
     header("Location: $app[redirect_success]?code=$code[code]&state=$state");
 }
 
-function token() {
+function handleAuthCode($input, $clientId)
+{
+    ['code' => $code] = $input;
+    $code = findCodeBy(["code"=> $code, 'client_id' => $clientId]);
+    if (!$code) {
+        throw new \RuntimeException(404);
+    }
+    if ($code['expires_at'] < time()) {
+        throw new \RuntimeException(400);
+    }
+    return $code['user_id'];
+}
+
+function handlePassword($input)
+{
+    ["username" => $username, "password" => $password] = $input;
+    $user = findUserBy(["username" => $username, 'password' => $password]);
+    if (!$user) {
+        throw new \RuntimeException(404);
+    }
+    return $user['id'];
+}
+
+function handleDefault()
+{
+    throw new \RuntimeException(400);
+}
+
+function token()
+{
     $input = $_SERVER['REQUEST_METHOD'] === "POST" ? $_POST : $_GET;
 
-    ['code' => $code, 'redirect_uri' => $redirect, 'client_id'=> $clientId, 'client_secret' => $clientSecret, 'grant_type' => $grantType] = $input;
+    ['redirect_uri' => $redirect, 'client_id'=> $clientId, 'client_secret' => $clientSecret, 'grant_type' => $grantType] = $input;
 
     $app = findAppBy([
         "client_id" => $clientId, "client_secret" => $clientSecret, 'redirect_success' => $redirect
@@ -110,30 +161,31 @@ function token() {
         http_response_code(404);
         return;
     }
-
-    $code = findCodeBy(["code"=> $code, 'client_id' => $clientId]);
-    if(!$code) {
-        http_response_code(404);
-        return;
-    }
-    if ($code['expires_at'] < time()) {
-        http_response_code(400);
-        return;
-    }
     
-    $token = [
-        'token' => bin2hex(random_bytes(16)),
-        'expires_at' => time() + (60*60*24*30),
-        'user_id' => $code['user_id'],
-        'client_id' => $code['client_id'],
-        'code' => $code['code']
-    ];
-    insertToken($token);
-    http_response_code(201);
-    echo json_encode(["access_token" => $token['token'], 'expire_in' => $token['expires_at']]);
+    try {
+        $userId = match ($grantType) {
+            "authorization_code" => handleAuthCode($input, $clientId),
+            "password" => handlePassword($input),
+            "client_credentials" => null,
+            default =>  handleDefault()
+        };
+
+        $token = [
+            'token' => bin2hex(random_bytes(16)),
+            'expires_at' => time() + (60*60*24*30),
+            'user_id' => $userId,
+            'client_id' => $clientId,
+        ];
+        insertToken($token);
+        http_response_code(201);
+        echo json_encode(["access_token" => $token['token'], 'expire_in' => $token['expires_at']]);
+    } catch (\RuntimeException $e) {
+        http_response_code(intval($e->getMessage()));
+    }
 }
 
-function me() {
+function me()
+{
     $headers = getallheaders();
     $auth = $headers['Authorization'] ?? $headers['authorization'] ?? null;
     [$type, $token] = explode(" ", $auth);
@@ -146,20 +198,18 @@ function me() {
         http_response_code(401);
         return;
     }
-    $code = findCodeBy(["code" => $token['code']]);
-    if (!$code) {
-        http_response_code(401);
-        return;
-    }
+    $user = findUserBy([
+        "id" => $token['user_id']
+    ]);
     echo json_encode([
         'user_id' => $token['user_id'],
-        'lastname' => 'Doe',
-        'firstname' => 'John'
+        'lastname' => $user['lastname'],
+        'firstname' => $user['firstname']
     ]);
 }
 
 $route = $_SERVER["REQUEST_URI"];
-switch(strtok($route, "?")) {
+switch (strtok($route, "?")) {
     case '/register':
         register();
         break;
@@ -171,6 +221,7 @@ switch(strtok($route, "?")) {
         break;
     case '/token':
         token();
+        break;
     case '/me':
         me();
         break;
